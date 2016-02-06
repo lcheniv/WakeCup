937fadaa095c34e6b61ffc16a0964a88cf2d0055
Spy Name Is Harry <lawrencechen@lawrences-mac.local>
2016-02-06 15:47:18 -0500

    Initial Commit

---
+++ Wake Up Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wake Up Final
+//
+//  Created by Lawrence Chen on 2/6/16.
+//  Copyright © 2016 Lawrence Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
